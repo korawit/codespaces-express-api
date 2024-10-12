a0fab0e6dd3ea85d409a12fa33ffc44cff2a3a21
korawit <korawit.orkphol@gmail.com>
2024-10-12 05:38:38 +0000

    connect to mysql

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 5000
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+const express = require('express');
+const bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+const app = express();
+const port = 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","*");
@@ -17,25 +19,44 @@ app.use((req,res,next)=>{
   );
   next();
 });
-app.use(express.json())
+app.use(express.json());
 
-const products=[
-  {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-  {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-  {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-  {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-  {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-  {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
+var con = mysql.createConnection({
+  host: "korawit.ddns.net=",
+  user: "webapp",
+  password: "password",
+  port: "3307",
+  database: "shop",
+});
+
+con.connect(function(err){
+  if(err) throw err;
+});
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.get('/api/products',(req,res)=>{
-  if(products.length>0)
-    res.send(products);
-  else
-    res.status(400).send("No products founds");
+  con.query("SELECT * FROM products",function(err,result,fields){
+    if(err) throw res.status(400).send("No products found");
+    console.log(result);
+    res.send(result);
+  });
+})
+
+app.get('/api/products/:id',(req,res)=>{
+  const id = req.params.id;
+  con.query(`SELECT * FROM products where id=${id}`,function(err,result,fields){
+    if(err) throw err;
+    if(result.length==0) 
+      res.status(400).send(`No products id: ${id} found`);
+    else{
+      console.log(result);
+      res.send(result);
+    }
+  });
 })
 
 app.listen(port, () => {
